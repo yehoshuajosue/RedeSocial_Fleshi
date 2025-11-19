33598800de3b11681e1c2dfb3220b9c5a03ada17
Josué Gumer <josuegumer364@gmail.com>
2025-11-19 16:47:40 -0300

    feat: initial setup of login and register

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
+from appfleshi import app
 
 if __name__ == '__main__':
     app.run(debug=True)
--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,15 +1,23 @@
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from appfleshi import app
+from appfleshi.forms import LoginForm, RegisterForm
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template('homepage.html')
+    login_form = LoginForm()
+    return render_template('homepage.html', form=login_form)
+
+@app.route('/createaccount', methods=['GET', 'POST'])
+def createaccount():
+    register_form = RegisterForm()
+    return render_template('createaccount.html', form=register_form)
 
 @app.route('/profile/<username>')
 @login_required
 def profile(username):
-    return render_template("profile.html", username=username)
+    return render_template('profile.html', username=username)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
